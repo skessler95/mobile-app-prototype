a96f98df433494684cb1d4e2ea22a25e4e01589d
Steven <Steven@Steven-HPdv6t_Q>
2017-01-18 19:01:01 -0500

    Mobile App

---
+++ src/mobile/app/ViewSplash.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mobile.app;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ *
+ * @author Steven
+ */
+public class ViewSplash extends JPanel {
+    private JLabel splashLabel;
+    private JLabel optionalLabel;
+    
+    ViewSplash(){
+    splashLabel = new JLabel(new ImageIcon());
+    add(splashLabel);
+    
+    optionalLabel = new JLabel("");
+    add(optionalLabel);
+    }
+}
